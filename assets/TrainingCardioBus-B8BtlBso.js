88abc6681e5cbcc972c6d323bbbe4609ba841cac
demianavol <yaminaasfak@gmail.com>
2026-08-31 11:23:46 +0300

    Updates

---
+++ assets/TrainingCardioBus-B8BtlBso.js
@@ -0,0 +1,1 @@
+import{B as e}from"./index-D5JPw7Wb.js";const o=new e("RUNNING"),a=new e({mode:"new",dayKey:"",dInd:null});export{o as c,a as t};
---
+++ assets/index-Dgs5-eTu.js
@@ -0,0 +1,1 @@
+import{bd as t}from"./index-D5JPw7Wb.js";function a(h){return t({attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M8 21h2v-3h6v-2h-6v-2h4.5c2.757 0 5-2.243 5-5s-2.243-5-5-5H9a1 1 0 0 0-1 1v7H5v2h3v2H5v2h3v3zm2-15h4.5c1.654 0 3 1.346 3 3s-1.346 3-3 3H10V6z"},child:[]}]})(h)}export{a as B};
